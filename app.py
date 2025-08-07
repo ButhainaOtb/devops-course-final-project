5054137dc185e0a2a98807f12bc4df53515eb26b
Buthaina Alotaibi <buthainao22@gmail.com>
2025-08-07 16:47:54 +0300

    final project

--- app.py
+++ app.py
@@ -1,28 +1,29 @@
-import os
-from flask import Flask, request, redirect, url_for
 import redis
+from flask import Flask, redirect, request, url_for
 
 app = Flask(__name__)
 r = redis.Redis(host="redis", port=6379)
 
 
-OPTIONS = ["Cats", "Dogs"]
+OPTIONS = ["Buthaina", "Reem"]
+
 
 @app.route("/", methods=["GET"])
 def index():
     votes = {option: int(r.get(option) or 0) for option in OPTIONS}
     return f"""
-    <h1>Vote for your favorite animal</h1>
+    <h1>Vote for your favorite Backend Engineer</h1>
     <form method="POST" action="/vote">
-        <button type="submit" name="vote" value="Cats">Cats</button>
-        <button type="submit" name="vote" value="Dogs">Dogs</button>
+        <button type="submit" name="vote" value="Buthaina">Buthaina</button>
+        <button type="submit" name="vote" value="Reem">Reem</button>
     </form>
     <h2>Current Results</h2>
-    <ul>
-        <li>Cats: {votes['Cats']}</li>
-        <li>Dogs: {votes['Dogs']}</li>
+    <ul>    
+        <li>Buthaina: {votes['Buthaina']}</li>
+        <li>Reem: {votes['Reem']}</li>
     </ul>
     """
+
 
 @app.route("/vote", methods=["POST"])
 def vote():
